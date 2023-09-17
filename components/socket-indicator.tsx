7f994e2f1966c2865c520891453edc89ddec4e5f
Ms0232 <180540106059@darshan.ac.in>
2023-09-17 18:17:12 +0530

    messages ui &

--- components/socket-indicator.tsx
+++ components/socket-indicator.tsx
@@ -9,13 +9,13 @@ export const SocketIndicator = () => {
   if (!isConnected) {
     return (
       <Badge variant="outline" className="bg-yellow-600 text-white border-none">
-        Fallback:Polling every 1s
+        Offline
       </Badge>
     );
   }
   return (
     <Badge variant="outline" className="bg-emerald-600 text-white border-none">
-      Live:Real-time updates
+      Online
     </Badge>
   );
 };
